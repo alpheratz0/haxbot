2e743a9601af41125ff80f2630352cad3830611e
alpheratz0 <alpheratz99@protonmail.com>
2020-12-11 15:05:00 -0300

    now room events are notified in the console

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,43 @@
 import { Player } from './api/player'
 import { Scores } from './api/scores'
+import { Logger, LoggerStyles } from './logger'
 import { room } from './room'
 
 // Room events
 
 room.onRoomLink = (url: string) => {  
+    Logger.logEvent('roomlink', url, new LoggerStyles('orange'));
 
 }
 
 room.onPlayerJoin = (player: Player) => {   
+    Logger.logEvent('playerjoin', player.name, new LoggerStyles('skyblue'));
 
 }
 
 room.onPlayerLeave = (player: Player) => {
+    Logger.logEvent('playerleave', player.name, new LoggerStyles('red'));
 
 }
 
 room.onStadiumChange = (newStadiumName: string, byPlayer: Player) => {
+    Logger.logEvent('stadiumchange', newStadiumName, new LoggerStyles('blue'));
 
 }
 
 room.onPlayerAdminChange = (changedPlayer: Player, byPlayer: Player) => {
+    Logger.logEvent('adminchange: ' + (changedPlayer.admin ? 'on' : 'off'), changedPlayer.name, new LoggerStyles('orange'));
 
 }
 
 room.onPlayerKicked = (kickedPlayer: Player, reason: string, ban: boolean, byPlayer: Player) => {
+    Logger.logEvent('playerkicked: ' + (ban ? 'ban' : 'kick'), kickedPlayer.name, new LoggerStyles('red'));
 
 }
 
 room.onPlayerChat = (player: Player, message: string) => { 
+    Logger.logEvent('playerchat', player.name + ': ' + message, new LoggerStyles('skyblue'));
+
     return true;
 }
 
