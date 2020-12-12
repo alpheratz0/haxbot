23a007ca784bdc94b2b6176cade454a7fec67c9a
alpheratz0 <alpheratz99@protonmail.com>
2020-12-12 16:35:30 -0300

    added abstract command class

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -34,3 +34,31 @@ export class CommandInput {
         return new CommandInput(cmd, args, flags);
     }
 }
+
+export abstract class CommandContext {
+    readonly args: string[];
+}
+
+export interface CommandCallback<TContext> {
+    (context: TContext): void;
+}
+
+export abstract class Command<TContext extends CommandContext> {
+    
+    /** The command name. */
+    public readonly name: string;
+
+    /** The command callback. */
+    public readonly run: CommandCallback<TContext>;
+    
+    constructor(name: string, callback: CommandCallback<TContext>) {
+        this.name = name.toLowerCase();
+        this.run = callback;
+    }
+
+    /** Tests the command, if the name matches it is executed with the context.
+     * @param name The name to test this command against.
+     * @param context The context where this command is executed.
+     */
+    abstract test(name: string, context: TContext): boolean;
+}
