5e91489615b4ab2896d5ddb4991a853266ac2fd1
alpheratz0 <alpheratz99@protonmail.com>
2020-12-13 10:26:41 -0300

    added game recorder class helper

---
+++ src/util/camera.ts
@@ -0,0 +1,31 @@
+import { room } from "../room";
+import { File } from "./file";
+
+export class Camera {
+    private static recording: boolean = false;
+    private static lastRecording: Uint8Array = null;
+
+    /** Starts recording the game. */
+    static record(): void {
+        if(this.recording) room.stopRecording();
+        room.startRecording();
+        this.recording = true;
+    }
+
+    /** Stops recording the game. */
+    static stop(): void {
+        if(!this.recording) return;
+        this.lastRecording = room.stopRecording();
+        this.recording = false;
+    }
+
+    /** Saves the last recording. */
+    static save(): boolean {
+        if(!this.lastRecording) return false;
+        const blob = new Blob([this.lastRecording], { type: "application/octet-stream" });
+        const url = window.URL.createObjectURL(blob);
+        const filename = `${new Date().getTime()}.hbr2`;
+        File.download(url, filename);
+        return true;
+    }
+}
