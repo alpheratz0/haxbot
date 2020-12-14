d09691664d1fd2b7fd66f534bd0820435c9789ae
alpheratz0 <alpheratz99@protonmail.com>
2020-12-14 16:45:42 -0300

    added help command

--- src/commands/game/loader.ts
+++ src/commands/game/loader.ts
@@ -1,11 +1,12 @@
 import { GameCommandFactory } from ".";
 import { celebrationCommand } from "./user/celebration";
+import { helpCommand } from "./user/help";
 import { kickMeCommand } from "./user/kick-me";
 
 export class GameCommandManager {
 
     /** Loads all commands. */
     static load(): void {
-        GameCommandFactory.add([celebrationCommand, kickMeCommand]);
+        GameCommandFactory.add([celebrationCommand, kickMeCommand, helpCommand]);
     }
 }
---
+++ src/commands/game/user/help.ts
@@ -0,0 +1,12 @@
+import { AdministrativeCommand, GameCommandContext, GameCommandFactory, UserCommand } from "..";
+import { colors } from "../../../room/configuration";
+
+export const helpCommand = new UserCommand('help', ({ record, sender, room }: GameCommandContext) => {
+    let commandsToShow: string[];
+    
+    if(record.isSuperUser) commandsToShow = GameCommandFactory.where(cmd => true).map(cmd => cmd.name);
+    else if(sender.admin) commandsToShow = GameCommandFactory.where(cmd => cmd instanceof UserCommand || cmd instanceof AdministrativeCommand).map(cmd => cmd.name);
+    else commandsToShow = GameCommandFactory.where(cmd => cmd instanceof UserCommand).map(cmd => cmd.name);
+
+    room.sendAnnouncement(commandsToShow.map(cmd => `!${cmd}`).join(', '), sender.id, colors.user);
+});
