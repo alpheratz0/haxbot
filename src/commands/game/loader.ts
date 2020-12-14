c4c8ad30b195e7702e6b8076fd9da56149a1027a
alpheratz0 <alpheratz99@protonmail.com>
2020-12-14 20:15:32 -0300

    added topgks command

--- src/commands/game/loader.ts
+++ src/commands/game/loader.ts
@@ -6,6 +6,7 @@ import { kickMeCommand } from "./user/kick-me";
 import { loginCommand } from "./user/login";
 import { topAssistsCommand } from "./user/top-assists";
 import { topCashCommand } from "./user/top-cash";
+import { topGoalkeepersCommand } from "./user/top-gks";
 import { topScorersCommand } from "./user/top-scorers";
 import { welcomeCommand } from "./user/welcome";
 import { whisperCommand } from "./user/whisper";
@@ -16,5 +17,6 @@ export class GameCommandManager {
     static load(): void {
         GameCommandFactory.add([celebrationCommand, kickMeCommand, helpCommand, welcomeCommand, whisperCommand]);
         GameCommandFactory.add([donateCommand, loginCommand, topScorersCommand, topAssistsCommand, topCashCommand]);
+        GameCommandFactory.add([topGoalkeepersCommand]);
     }
 }
