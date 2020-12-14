571bd7f57364a98f9a336f7729b5bdd494ac8a42
alpheratz0 <alpheratz99@protonmail.com>
2020-12-14 16:49:39 -0300

    added welcome command

--- src/commands/game/loader.ts
+++ src/commands/game/loader.ts
@@ -2,11 +2,12 @@ import { GameCommandFactory } from ".";
 import { celebrationCommand } from "./user/celebration";
 import { helpCommand } from "./user/help";
 import { kickMeCommand } from "./user/kick-me";
+import { welcomeCommand } from "./user/welcome";
 
 export class GameCommandManager {
 
     /** Loads all commands. */
     static load(): void {
-        GameCommandFactory.add([celebrationCommand, kickMeCommand, helpCommand]);
+        GameCommandFactory.add([celebrationCommand, kickMeCommand, helpCommand, welcomeCommand]);
     }
 }
---
+++ src/commands/game/user/welcome.ts
@@ -0,0 +1,10 @@
+import { GameCommandContext, UserCommand } from "..";
+import { LanguageProvider } from "../../../langs/language-provider";
+import { colors } from "../../../room/configuration";
+import { PlayerDB } from "../../../storage/player-db";
+
+export const welcomeCommand = new UserCommand('welcome', ({ record, args, sender, room }: GameCommandContext) => {
+    record.welcomeMessage = args.join(" ").trim() || null;
+    room.sendAnnouncement(LanguageProvider.get('Welcome message updated.'), sender.id, colors.user);
+    PlayerDB.update(record);
+});
