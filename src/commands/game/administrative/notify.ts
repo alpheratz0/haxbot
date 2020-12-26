56223def1c420dc25e93f063d123edf5a12ce47a
alpheratz0 <alpheratz99@protonmail.com>
2020-12-26 00:49:43 -0300

    added notify command

---
+++ src/commands/game/administrative/notify.ts
@@ -0,0 +1,14 @@
+import { AdministrativeCommand, GameCommandContext } from "..";
+import { LanguageProvider } from "../../../langs/language-provider";
+import { colors } from "../../../room/configuration";
+import { Color } from "../../../util/color";
+
+export const notifyCommand = new AdministrativeCommand('notify', ({ args, room, sender }: GameCommandContext) => {
+    const message = args.join(" ").trim();
+    const color = Color.random();
+
+    if(message.length == 0) 
+        return room.sendAnnouncement(LanguageProvider.get('Message is too short.'), sender.id, colors.error);
+
+    room.sendAnnouncement(message, undefined, color, "normal", 2);
+});
