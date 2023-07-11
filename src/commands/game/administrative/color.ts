5f16a2ef2f678a974dabc0e99073b143f7440f71
alpheratz0 <alpheratz99@protonmail.com>
2023-07-11 09:36:28 -0300

    allow custom colors for !color command

--- src/commands/game/administrative/color.ts
+++ src/commands/game/administrative/color.ts
@@ -5,9 +5,19 @@ import { Color } from '../../../util/color';
 
 export const colorCommand = new AdministrativeCommand(
 	'color',
-	({ sender, record, room }: GameCommandContext) => {
-		const color = (record.color = Color.random());
+	({ sender, args, record, room }: GameCommandContext) => {
+		let color = NaN;
+
+		if (args.length > 0) {
+			color = parseInt(`0x${args.shift()}`);
+		}
+
+		if (isNaN(color)) {
+			color = Color.random();
+		}
+
 		const colorHtml = Color.toHTML(color);
+		record.color = color;
 
 		PlayerDB.update(record).then(() => {
 			room.sendAnnouncement(
